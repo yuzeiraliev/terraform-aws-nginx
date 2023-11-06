b42f495e1d71e36d80ffdb561902e8fcfa58060a
yuzeiraliev <141018040+yuzeiraliev@users.noreply.github.com>
2023-11-06 09:52:36 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+module "ec2" {
+  source  = "app.terraform.io/yuzeir-org/ec2/aws"
+  version = "1.0.0"
+  ec2_ami = var.ec2_ami
+  instance_type = var.instance_type
+  aws_private_subnet_id = module.networking.aws_private_subnet_id
+  aws_security_group_id = module.sg.aws_security_group_id
+}
+
+module "network" {
+  source  = "app.terraform.io/yuzeir-org/network/aws"
+  version = "1.0.0"
+  cidr_block = var.cidr_block
+  private_subnet_cidr = var.private_subnet_cidr
+  public_subnet_cidr_1 = var.public_subnet_cidr_1
+  public_subnet_cidr_2 = var.public_subnet_cidr_2
+  private_az = var.private_az
+  public_az1 = var.public_az1
+  public_az2 = var.public_az2
+}
+
+module "lb" {
+  source  = "app.terraform.io/yuzeir-org/lb/aws"
+  version = "1.0.0"
+  vpc_id = module.networking.vpc_id
+
+  aws_subnet1_id = module.networking.aws_subnet1_id
+  aws_subnet2_id = module.networking.aws_subnet2_id
+
+  ec2_id = module.ec2.ec2_id
+
+  aws_security_group_id = module.sg.aws_security_group_id
+
+  app_port = var.app_port
+  app_protocol = var.app_protocol
+}
+
+module "sg" {
+  source  = "app.terraform.io/yuzeir-org/sg/aws"
+  version = "1.0.0"
+  vpc_id = module.networking.vpc_id
+  cidr_block_allow = var.cidr_block_allow
+  app_port = var.app_port
+}
