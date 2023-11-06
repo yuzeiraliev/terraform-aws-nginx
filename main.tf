f0d2a2a47154ae284183823865c3342e03b8966a
yuzeiraliev <141018040+yuzeiraliev@users.noreply.github.com>
2023-11-06 10:19:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "ec2" {
   version = "1.0.0"
   ec2_ami = var.ec2_ami
   instance_type = var.instance_type
-  aws_private_subnet_id = module.networking.aws_private_subnet_id
+  aws_private_subnet_id = module.network.aws_private_subnet_id
   aws_security_group_id = module.sg.aws_security_group_id
 }
 
@@ -22,10 +22,10 @@ module "network" {
 module "lb" {
   source  = "app.terraform.io/yuzeir-org/lb/aws"
   version = "1.0.0"
-  vpc_id = module.networking.vpc_id
+  vpc_id = module.network.vpc_id
 
-  aws_subnet1_id = module.networking.aws_subnet1_id
-  aws_subnet2_id = module.networking.aws_subnet2_id
+  aws_subnet1_id = module.network.aws_subnet1_id
+  aws_subnet2_id = module.network.aws_subnet2_id
 
   ec2_id = module.ec2.ec2_id
 
@@ -38,7 +38,7 @@ module "lb" {
 module "sg" {
   source  = "app.terraform.io/yuzeir-org/sg/aws"
   version = "1.0.0"
-  vpc_id = module.networking.vpc_id
+  vpc_id = module.network.vpc_id
   cidr_block_allow = var.cidr_block_allow
   app_port = var.app_port
 }
